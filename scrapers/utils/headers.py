ee1a0986d2a7c13dd20742bf3634a07c00e903ed
dunossauro <mendesxeduardo@gmail.com>
2022-08-29 19:08:58 -0300

    Criando headers padronizados

---
+++ scrapers/utils/headers.py
@@ -0,0 +1,14 @@
+from random import randrange
+
+UAS = (
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
+    'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0',
+    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36',
+)
+
+ua = UAS[randrange(len(UAS))]
+
+headers = {'user-agent': ua}
